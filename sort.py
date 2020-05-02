8a730b35e589b06e9677e9bf9a0d5490d4d8345a
Pavel Fokin <fokinpv@gmail.com>
2020-05-02 14:44:20 +0300

    Add insertion sort

--- sort.py
+++ sort.py
@@ -12,3 +12,13 @@ def bubble(array: list, reverse=False) -> list:
                 array[i-1], array[i] = array[i], array[i-1]
                 swapped = True
     return array
+
+
+def insertion(array: list, reverse=False) -> list:
+    cmp = operator.lt if reverse else operator.gt
+    n = len(array)  # pylint:disable=invalid-name
+    for i in range(n+1):
+        for j in reversed(range(1, i)):
+            if cmp(array[j-1], array[j]):
+                array[j-1], array[j] = array[j], array[j-1]
+    return array
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,27 +1,29 @@
-from sort import bubble
+from sort import bubble, insertion
 
 
-def test_sort_success():
-    res = bubble([5, 4, 3, 2, 1])
+def test_sort_success(method):
+    res = method([5, 4, 3, 2, 1])
     assert res == [1, 2, 3, 4, 5], res
 
-    res = bubble([5, 5, 2, 3, 1])
+    res = method([5, 5, 2, 3, 1])
     assert res == [1, 2, 3, 5, 5], res
 
 
-def test_sort_reverse():
-    res = bubble([1, 2, 3, 4, 5], reverse=True)
+def test_sort_reverse(method):
+    res = method([1, 2, 3, 4, 5], reverse=True)
     assert res == [5, 4, 3, 2, 1], res
 
 
-def test_sort_one():
-    assert bubble([1]) == [1]
+def test_sort_one(method):
+    assert method([1]) == [1]
 
 
-def test_sort_empty():
-    assert bubble([]) == []
+def test_sort_empty(method):
+    assert method([]) == []
 
 
-test_sort_success()
-test_sort_reverse()
-test_sort_empty()
+for method in (bubble, insertion):
+    test_sort_success(method)
+    test_sort_reverse(method)
+    test_sort_empty(method)
+    test_sort_one(method)
